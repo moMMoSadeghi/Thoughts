cd478c95ebbd6dabeeff0fe571433c48f60b82df
Moei <mohammad_sadeghi96@yahoo.com>
2021-09-09 12:22:33 +0200

    Initial Commit

---
+++ Thoughts/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Thoughts
+//
+//  Created by Mohammadreza Sadeghi on 09/09/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
